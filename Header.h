e480300d4a5e413a93efe95ed76626624d03b1c6
greenhouse <adamdx1337@outlook.com>
2024-10-30 22:51:50 +1100

    Update Header.h

--- Header.h
+++ Header.h
@@ -12,12 +12,44 @@ inline std::string& CString::assign(InputIterator first, InputIterator last)
     return _str;              
 };
 
-struct CAddPlayerStruct {
-    CString _User
-    CString _Name
-    int MachineID
-    // 2 vars, SocialID + Unknown, too lazy to write...
-    bool HotJoin
+
+struct CPersistentVtbl
+{
+	/*void* (__fastcall* __vecDelDtor)(unsigned int);
+	void(__fastcall* Write)(CWriter*);
+	void(__fastcall* WriteMembers)(CWriter*);
+	void(__fastcall* Read)(CReader*);
+	void(__fastcall* ReadMember)(CReader*, int);
+	void(__fastcall* InitPostRead)(CString*, int, int);
+	void(__fastcall* InitPostRead)();*/
+};
+
+struct CPersistent
+{
+	CPersistentVtbl* vfptr;
+	int _TypeToken;
+};
+
+struct CCommand : CPersistent
+{
+	bool _bLogged;
+	int _nCommandSender; 
+	unsigned int _nRecipient; 
+	unsigned __int16 _nRecipientPort;
+	unsigned __int16 _nTickStamp;
+	bool _bTargetedAsynchronous;
+	bool _bMayBeRemoved;
+	unsigned int _nIdentity;
+};
 
 
+
+struct CAddPlayerCommand : CCommand
+{
+	CString* _User; //hoi
+	CString* _Name;
+	//DWORD UNKNOWN;
+	int _nMachineId;
+	bool _bHotjoin;
+	__int64 Social;
 };
